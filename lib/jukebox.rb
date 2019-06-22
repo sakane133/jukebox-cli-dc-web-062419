a8a1b476639ec0dd1eb126e6c6731e548a9bea6f
Samantha Kane <s.kane133@gmail.com>
2019-06-22 16:04:24 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+def songs
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +10,66 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+end
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  response = gets.chomp
+    valid = false
+  songs.each_with_index do |song, index|
+    if (response.to_i - 1 == index)
+      puts "Playing #{songs[index]}"
+      valid = true
+    elsif (response == song)
+      puts "Playing #{response}"
+      valid = true
+    end
+  end
+  puts "Invalid input, please try again" if valid == false
+end
+
+
+def list (music)
+  music.each { |song, location|
+  puts song}
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+   help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  while user_response != "exit"
+    case user_response
+      when "help"
+        help
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "list"
+        list(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "play"
+        play(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      else
+        puts "Invalid input, please try again"
+        user_response = gets.chomp
+    end
+  end
+  exit_jukebox
+end 
+
+  
